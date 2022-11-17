0ccfe43521eb8b3dfa00b81aee3915ce785e0b1e
RocioAlbarran <rocio.ortiz.a@gmail.com>
2022-11-16 18:28:44 -0600

    link autor

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,11 @@ from django.shortcuts import redirect
 
 
 # Create your views here.
+def post_author(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	posts = Post.objects.filter(author=post.author)
+	return render(request, 'blog/post_list.html', {'posts' : posts})
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
